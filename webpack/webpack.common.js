8326740794be4a652a8ae0b2cfbc3730aa4819a5
Alfredoeb9 <alfredoeb96@gmail.com>
2024-05-23 15:59:12 -0700

    [inital] starter file

---
+++ webpack/webpack.common.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: [
+    "core-js/modules/es.array.iterator",
+    "core-js/stable/symbol",
+    "core-js/stable/object",
+    "./src/index.tsx",
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+  },
+  target: ["browserslist"],
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(css)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg)$/i,
+        type: "asset/inline",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    modules: ["src", "node_modules"],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      manifest: "./public/manifest.json",
+      favicon: "./public/favicon.ico",
+    }),
+  ],
+};
