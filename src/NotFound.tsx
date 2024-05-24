d462f9aa981680187d368edfc86a40a814d63e51
Alfredoeb9 <alfredoeb96@gmail.com>
2024-05-24 11:24:10 -0700

    [NotFound] created NotFound.tsx page

---
+++ src/NotFound.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+function NotFound() {
+  window.document.title = "GeoLocation — Not Found";
+
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div>
+        <Link to="/">
+          <button type="button">🏡 Back to Home</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
