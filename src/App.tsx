54e008b07f290fb6861098967f27da40e954737f
Alfredoeb9 <alfredoeb96@gmail.com>
2024-05-24 11:47:06 -0700

    [login] added in login page to router

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,14 @@ import Home from "./pages/Home";
 import "react-toastify/dist/ReactToastify.css";
 import SignUp from "./pages/SignUp";
 import NotFound from "./NotFound";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
       <Route path="*" element={<NotFound />} />
     </Route>
   )
