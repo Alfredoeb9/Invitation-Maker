64d51ab5fec528b78e47c1bb80635a7c8dd9077d
Alfredoeb9 <alfredoeb96@gmail.com>
2024-05-28 14:30:24 -0700

    [clean up]

--- src/components/PrivateRoutes.tsx
+++ src/components/PrivateRoutes.tsx
@@ -6,7 +6,6 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 export default function PrivateRoutes() {
   const location = useLocation();
   const user = useAppSelector(isUserLoggedIn);
-  const user2 = useAppSelector((state) => state.user.email);
 
   return user.length > 0 ? (
     <Outlet />
