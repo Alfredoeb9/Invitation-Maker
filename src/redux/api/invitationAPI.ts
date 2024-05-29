808ce0e1f967b5f00cabf093b9518564056e10d7
Alfredoeb9 <alfredoeb96@gmail.com>
2024-05-28 20:41:36 -0700

    [api] fixed api url

--- src/redux/api/invitationAPI.ts
+++ src/redux/api/invitationAPI.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const invitationsApi = createApi({
   reducerPath: "invitationApi",
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }),
   tagTypes: ["invitations"],
   endpoints: (builder) => ({
     addInvitation: builder.mutation({
